f214b39f5acc2b6902eac1c840eee39873ca1ed9
mariyemaGuen <132705196+mariyemaGuen@users.noreply.github.com>
2023-07-24 01:15:01 +0100

    Update size.c

--- size.c
+++ size.c
@@ -2,6 +2,7 @@
 
 /**
  * get_size - Calculates the size to cast the argument
+ * I edited this file
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
