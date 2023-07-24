b2b9c54becada3d9d922fcf4726d540ee2aa356e
mariyemaGuen <132705196+mariyemaGuen@users.noreply.github.com>
2023-07-24 01:06:02 +0100

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -2,6 +2,7 @@
 
 /**
  * get_width - Calculates the width for printing
+ * I edited this file
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
